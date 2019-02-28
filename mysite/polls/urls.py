4f2cf1d0a0274d1d66ceea9c83303e4b58b73ac4
kkron <knroro1877887@outlook.jp>
2019-02-28 19:41:58 +0900

    finished 3page

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-
+from django.urls import path,include
+from django.contrib import admin
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('<int:question_id>/', views.detail, name='detail'),
-        path('<int:question_id>/results/', views.results, name='results'),
-        path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('admin/', admin.site.urls),
 ]
